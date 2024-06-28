a79a7bef9068d476ebefce3eb5420419ac207aa8
AJ-Appiah <ericapph@gmail.com>
2024-06-28 08:52:56 +0000

    Creating an Express App

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Recipes from "./assets/pages/Recipes"
+import Welcome from "./assets/pages/Welcome"
+import Categories from "./assets/pages/Categories";
+
+const router = createBrowserRouter([
+  {path: '/', element: <Welcome/>},
+  {path: '/recipes', element: <Recipes/>},
+  {path: '/categories', element: <Categories/>}
+]);
+
+function App() {
+
+  return (
+    <>
+    <RouterProvider router={router}/>
+    </>
+  
+  )
+}
+
+export default App
